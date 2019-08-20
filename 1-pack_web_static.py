4bbfbe3b1af6cb52aafa2335725358cad956b51a
andres-martin <andresmartin266@gmail.com>
2019-08-20 23:10:28 +0000

    file to compress web_static folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' compresses a folder '''
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    ''' method that compresses
+        files to a .tgz format
+    '''
+    date = datetime.now().strftime('%Y%m%d%H%M%S')
+    local("mkdir -p versions/")
+    try:
+        local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+        return "/versions/web_static_{}.tgz".format(date)
+    except Exception:
+        return None
