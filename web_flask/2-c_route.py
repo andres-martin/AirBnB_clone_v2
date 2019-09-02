cba5790683db081e3b649c90c0a8d6c008ee9a02
andres-martin <andresmartin266@gmail.com>
2019-09-02 16:59:37 +0000

    c route with params

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' script that starts a Flask web application '''
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    ''' renders a message '''
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' renders a message '''
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_param(text):
+    ''' renders a message with a q param '''
+    return "C %s" % escape(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
